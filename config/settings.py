dc6c9549357858c04166bdf12c86cfd10f0c4886
jaruco <jruiz.jc@gmail.com>
2026-01-16 12:06:34 +0100

    requirements settings2

--- config/settings.py
+++ config/settings.py
@@ -67,7 +67,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 DATABASES = {
     'default': dj_database_url.config(
-        default=str(config('DATABASE_URL')),
+        default=config('DATABASE_URL', default='sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
