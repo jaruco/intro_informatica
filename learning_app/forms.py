7f07e4419e1fdf319c0b30446f4f134a85064d71
jaruco <jruiz.jc@gmail.com>
2026-01-18 10:21:07 +0100

    fix on responsive mode

--- learning_app/forms.py
+++ learning_app/forms.py
@@ -7,22 +7,26 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(
         max_length=254,
         required=True,
+        label='Correo Electrónico',
         help_text='Obligatorio. Introduce una dirección de correo electrónico válida.',
         widget=forms.EmailInput(attrs={'class': 'form-control'})
     )
     username = forms.CharField(
         max_length=150,
         required=True,
+        label="Nombre de Usuario",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     first_name = forms.CharField(
         max_length=30,
         required=False,
+        label="Nombre",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
     last_name = forms.CharField(
         max_length=30,
         required=False,
+        label="Apellido",
         widget=forms.TextInput(attrs={'class': 'form-control'})
     )
 
